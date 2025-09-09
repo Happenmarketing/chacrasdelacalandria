f14e5993b4448cb553fdbb651f07fb33771f945e
Happenmarketing <hola@happenmarketing.com>
2025-09-09 12:25:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'  // 👈 aquí el cambio
+import react from '@vitejs/plugin-react-swc'   // usa el plugin correcto
+import { fileURLToPath, URL } from 'node:url'  // para el alias @
 
 export default defineConfig({
   plugins: [react()],
-  base: '/chacrasdelacalandria/',  // dejamos esto así por ahora
+  base: '/chacrasdelacalandria/',              // lo dejamos así por ahora
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // alias @ -> /src
+    },
+  },
 })
